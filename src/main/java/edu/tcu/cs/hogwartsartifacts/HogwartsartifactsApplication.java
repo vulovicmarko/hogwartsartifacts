0fd34f4bc49c5545faa011c9db152a1bfec3a23e
unknown <m.vulovic@tcu.edu>
2020-05-10 12:34:49 -0500

    Initial commit

---
+++ src/main/java/edu/tcu/cs/hogwartsartifacts/HogwartsartifactsApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsartifactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsartifactsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/hogwartsartifacts/HogwartsartifactsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsartifactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
